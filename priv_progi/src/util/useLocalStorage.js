6f30f0ee106e4e651fc283e63dc62dd879e836ef
BGOFFICIAL <128068299+BGOFFICIAL@users.noreply.github.com>
2024-01-16 06:08:33 +0100

    Popravljeno usmjeravanje po rolovima,dodane stranice
    Addequipment i AddRooms dodan uz popravljeno usmjeravanje

---
+++ priv_progi/src/util/useLocalStorage.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function useLocalState(defaultValue, key) {
+  const [value, setValue] = useState(() => {
+    const stickyValue = localStorage.getItem(key);
+
+    if (
+      stickyValue === "undefined" ||
+      stickyValue === "null" ||
+      stickyValue === null ||
+      stickyValue === "" ||
+      stickyValue === " " ||
+      stickyValue === undefined ||
+      stickyValue === '"' ||
+      stickyValue === '""' 
+    ) {
+      return defaultValue;
+    }
+
+    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+  });
+
+  useEffect(() => {
+    if (value !== undefined || value !== null)
+      localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
+
+export { useLocalState };
